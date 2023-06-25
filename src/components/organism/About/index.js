b719229262af91c2508e7a35db824972ecdc1b22
Zulfi Rizkiawan <68777448+zulfirizkiawan@users.noreply.github.com>
2023-06-25 17:56:03 +0700

    New MainBanner

--- src/components/organism/About/index.js
+++ src/components/organism/About/index.js
@@ -12,57 +12,59 @@ export default function About() {
   }, []);
   return (
     <div id="about" className="w-full p-2 flex flex-col py-16 px-10 ">
-      <div className="w-full">
-        <p className="uppercase text-xl tracking-widest text-[#035B81] pt-10">
-          About Me
-        </p>
-        <h2 className="py-4">Siapa saya?</h2>
-        <div className="w-[13%] bg-[#035B81] h-2" />
-      </div>
-
-      <div className="grid lg:grid-cols-5 md:grid-cols-2 mt-5 gap-10">
-        <div
-          className="col-span-3 lg:col-span-2 w-full h-full flex justify-center items-center"
-          data-aos="fade-up"
-          data-aos-offset="150"
-        >
-          <Image
-            src={aboutPhoto}
-            className="rounded-[5%] h-[320px] w-[320px]"
-            alt="/"
-          />
+      <div className="max-w-[1240px] mx-auto px-2 py-10">
+        <div className="w-full">
+          <p className="uppercase text-xl tracking-widest text-[#035B81] pt-10">
+            About Me
+          </p>
+          <h2 className="py-4">Siapa saya?</h2>
+          <div className="w-[13%] bg-[#035B81] h-2" />
         </div>
 
-        <div className="col-span-3 w-full h-auto">
-          <p className="text-gray-600 text-justify">
-            Halo, nama saya Zulfi Rizkiawan. Saya lulusan dari STIKI Malang
-            dengan program studi Teknik Informatika. Saya seorang Front-end
-            Developer dan Mobile Developer, saya memiliki keahlian dan
-            pengalaman di bidang pengembangkan aplikasi mobile dan desain web
-            yang responsif dan menarik. Saya berdedikasi untuk menciptakan
-            pengalaman pengguna yang optimal melalui desain yang intuitif dan
-            fungsional.
-          </p>
+        <div className="grid lg:grid-cols-5 md:grid-cols-2 mt-5 gap-10">
           <div
-            className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-8 text-center"
+            className="col-span-3 lg:col-span-2 w-full h-full flex justify-center items-center"
             data-aos="fade-up"
-            data-aos-offset="200"
+            data-aos-offset="150"
           >
-            <div className="flex flex-col w-full py-10 shadow-lg shadow-gray-300 p-3 rounded-lg justify-center items-center ">
-              <BiMedal size={30} />
-              <h6 className="mt-2">Pengalaman</h6>
-              <p className="mt-2 text-gray-500">Kurang dari 1 tahun </p>
-            </div>
-            <div className="flex flex-col w-full py-10 shadow-lg shadow-gray-300 p-3 rounded-lg justify-center items-center ">
-              <BiCodeBlock size={30} />
-              <h6 className="mt-2">Menyelesaikan</h6>
-              <p className="mt-2 text-gray-500">8 Project</p>
-            </div>
-            <div className="flex flex-col w-full py-10 shadow-lg shadow-gray-300 p-3 rounded-lg justify-center items-center ">
-              <BiSupport size={30} />
-              <h6 className="mt-2">Bantuan</h6>
-              <p className="mt-2 text-gray-500">Senin - Jumat </p>
-              <p className="text-gray-500">08:00 - 16:00 </p>
+            <Image
+              src={aboutPhoto}
+              className="rounded-[5%] h-[320px] w-[320px]"
+              alt="/"
+            />
+          </div>
+
+          <div className="col-span-3  h-auto">
+            <p className="text-gray-600 text-justify">
+              Halo, nama saya Zulfi Rizkiawan. Saya lulusan dari STIKI Malang
+              dengan program studi Teknik Informatika. Saya seorang Front-end
+              Developer dan Mobile Developer, saya memiliki keahlian dan
+              pengalaman di bidang pengembangkan aplikasi mobile dan desain web
+              yang responsif dan menarik. Saya berdedikasi untuk menciptakan
+              pengalaman pengguna yang optimal melalui desain yang intuitif dan
+              fungsional.
+            </p>
+            <div
+              className="grid grid-cols-1 md:grid-cols-3 gap-5 mt-8 text-center"
+              data-aos="fade-up"
+              data-aos-offset="200"
+            >
+              <div className="flex flex-col  py-10 shadow-lg shadow-gray-300 p-3 rounded-lg justify-center items-center ">
+                <BiMedal size={30} />
+                <h6 className="mt-2">Pengalaman</h6>
+                <p className="mt-2 text-gray-500">Kurang dari 1 tahun </p>
+              </div>
+              <div className="flex flex-col  py-10 shadow-lg shadow-gray-300 p-3 rounded-lg justify-center items-center ">
+                <BiCodeBlock size={30} />
+                <h6 className="mt-2">Menyelesaikan</h6>
+                <p className="mt-2 text-gray-500">8 Project</p>
+              </div>
+              <div className="flex flex-col  py-10 shadow-lg shadow-gray-300 p-3 rounded-lg justify-center items-center ">
+                <BiSupport size={30} />
+                <h6 className="mt-2">Bantuan</h6>
+                <p className="mt-2 text-gray-500">Senin - Jumat </p>
+                <p className="text-gray-500">08:00 - 16:00 </p>
+              </div>
             </div>
           </div>
         </div>
--- src/components/organism/MainBanner/index.js
+++ src/components/organism/MainBanner/index.js
@@ -1,28 +1,46 @@
 import { ContactItem } from "@/components/molecules";
+import Image from "next/image";
+import aboutPhoto from "public/assets/aku.png";
+import React, { useRef } from "react";
 
 export default function MainBanner() {
+  const ref = useRef(null);
+  React.useEffect(() => {
+    import("@lottiefiles/lottie-player");
+  });
   return (
     <div id="home" className="w-full h-screen text-center pt-10">
-      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
-        <div>
-          <p className="uppercase text-sm tracking-widest text-gray-600">
-            AYO BANGUN SESUATU BERSAMA
-          </p>
-          <h2 className="">
-            Hi, Saya <span className="text-[#035B81]">Zulfi Rizkiawan</span>
-          </h2>
-          <h2 className="py-2 text-gray-700">
-            Mobile Developer & Front-End Web Developer
-          </h2>
-          <p className="py-4 text-gray-600 max-w-[70%] m-auto">
-            Sebagai seorang Mobile Developer dan Front-end Developer, saya
-            memiliki keahlian dan pengalaman dalam mengembangkan aplikasi mobile
-            dan desain web yang responsif dan menarik. Saya berdedikasi untuk
-            menciptakan pengalaman pengguna yang optimal melalui desain yang
-            intuitif dan fungsional.
-          </p>
-          <div className="px-10">
-            <ContactItem />
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex items-center">
+        <div className="grid lg:grid-cols-5 md:grid-cols-2 mt-5 gap-5">
+          <div className="col-span-3 w-full h-auto pt-40 md:pt-0 ">
+            <p className="uppercase text-sm tracking-widest text-gray-600 ">
+              AYO BANGUN SESUATU BERSAMA
+            </p>
+            <h2 className="">
+              Hi, Saya <span className="text-[#035B81]">Zulfi Rizkiawan</span>
+            </h2>
+            <h3 className="py-2 text-gray-700 text-2xl">
+              Mobile Developer & Front-End Web Developer
+            </h3>
+            <p className="py-4 text-gray-600 max-w-[70%] m-auto">
+              Setiap commit di repository GitHub adalah langkah maju dalam
+              perjalanan sebagai seorang pengembang. Terus memperluas
+              pengalaman, karena setiap kode yang ditulis membuka wawasan besar.
+            </p>
+            <div className="px-10">
+              <ContactItem />
+            </div>
+          </div>
+          <div className="col-span-3 lg:col-span-2 w-full h-full flex justify-center items-center">
+            <lottie-player
+              id="firstLottie"
+              ref={ref}
+              autoplay
+              loop
+              mode="normal"
+              src="https://assets9.lottiefiles.com/packages/lf20_0yfsb3a1.json"
+              style={{ width: "350px", height: "350px" }}
+            ></lottie-player>
           </div>
         </div>
       </div>
